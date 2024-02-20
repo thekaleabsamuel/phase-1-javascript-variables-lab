84570d7c614d2bb55a6a8b3898c3268f4c0e11d5
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-02-20 14:20:57 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+// Define companyName as a const
+const companyName = "Scuber";
+
+// Define mostProfitableNeighborhood as a const and set it equal to 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define companyCeo as a const and set it equal to 'Susan Smith'
+let companyCeo = 'Susan Smith';
